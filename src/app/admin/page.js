3b206f86180f80b456b710087e0bb419643d66a1
Nazca13 <nazcafromzero13@gmail.com>
2026-04-18 03:47:54 +0700

    debuggig

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,7 +1,6 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/lib/supabase'
 import StatusBadge from '@/components/StatusBadge'
